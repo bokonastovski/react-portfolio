1c86f6f5e942af66a04a953ddc332f1433d36597
Borjan Nastovski <borjan00@yahoo.com>
2024-11-04 21:53:38 +0100

    //

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -17,7 +17,7 @@ const About = () => {
           className="w-full lg:w-1/2 lg:p-8"
         >
           <div className="flex items-center justify-center">
-            <img className="rounded-2xl" src="/images/profilePic.png" alt="" />
+            <img className="rounded-2xl" src="/images/aboutme.jpg" alt="" />
           </div>
         </motion.div>
         <motion.div
